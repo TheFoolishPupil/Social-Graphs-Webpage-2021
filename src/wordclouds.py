c22d3e487749e6710a127ca5e892c9da67285796
melmel6 <melina.siskou@gmail.com>
2021-12-08 19:53:20 +0100

    ui changes in wordclouds

--- src/wordclouds.py
+++ src/wordclouds.py
@@ -1,16 +1,21 @@
 import streamlit as st
 from constants import GENRE_TF_IDF_DATA
+import ast
 
 def render_genre_tf_idf():
     st.text("")
     st.write(f"### Top 10 words according to TF:")
-    for index, row in GENRE_TF_IDF_DATA.head(5).iterrows():
-        st.write(f"**{row.Genre}** : {row.Top_10_TF}")
+    for index, row in GENRE_TF_IDF_DATA.head(13).iterrows():
+        li = ast.literal_eval(row.Top_10_TF)
+        words = ', '.join(li)
+        st.write(f"**{row.Genre}** : {words}")
 
     st.text("")
-    st.write(f"**### Top 10 words according to TF_IDF:**")
-    for index, row in GENRE_TF_IDF_DATA.head(5).iterrows():
-        st.write(f"**{row.Genre}** : {row.Top_10_TF_IDF}")
+    st.write(f"### Top 10 words according to TF-IDF:")
+    for index, row in GENRE_TF_IDF_DATA.head(13).iterrows():
+        li = ast.literal_eval(row.Top_10_TF_IDF)
+        words = ', '.join(li)
+        st.write(f"**{row.Genre}** : {words}")
 
 def render_word_clouds():
     """Renders wordclouds on the page."""
@@ -134,3 +139,34 @@ def render_word_clouds():
             "<h3 style='text-align: center; '>Western</h3>",
             unsafe_allow_html=True,
         )
+
+def render_wordcloud_text():
+    st.write(f"From the generated wordclouds we can see that almost all genres are mostly "
+    "described by the name of their characters, same way as before. Again, we "
+    "would ideally want names to be filtered out so that we could comment on the"
+    " language used in different genres. This wasn't practically feasible so we "
+    "need to interperet the wordclouds differently. Instead of telling us about "
+    "the general language used in a particular genre, the wordclouds tell us, "
+    "through the character names, which film titles are the most popular in a "
+    "genre and have become cult favorites.")
+
+    st.text("")
+    st.write(f"**Shrek** has become a very popular movie and has attained a cultural "
+    "  status. This can be seen from our worldclouds, since Shrek is one of "
+    "  the top words in genres *Romance, Family, Animation, Fantasy*")
+
+    st.text("")
+    st.write(f"**Katniss** is also a very prevelant character name throughout our wordclouds."
+    "  This also makes sense, since Katniss is the protagonist of the *Hunger Games*"
+    "  series which consists of four movies and had a large fanbase.")
+
+    st.text("")
+    st.write(f"Same goes for **Bella** and **Edward**, the main characters of the *Twilight* "
+    " *series* but also **Stark, Thor, Logan** from the *Avenges* and **Frodo, Gandalf**"
+    "  from *The Lord of the Rings*. It seems movie series have a great influence over"
+    "  our wordclouds.")
+
+    st.text("")
+    st.write(f"Since **Jackass** is the only movie in the *Documentary* category we get"
+    " a wordcloud only from its own plot. This is why we have more non-character"
+    " words in this wordcloud.")
